558ace427a4a0e493ebb7b7176d8ac19351eaa49
Janhvi Pardeshi <janhvip.etc20@sbjit.edu.in>
2024-02-02 14:06:54 +0530

    janhvi

--- src/pages/_auth/forms/LoaderPage.jsx
+++ src/pages/_auth/forms/LoaderPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import PreLoader from '../components/PreLoader'
+import PreLoader from '../../../components/PreLoader'
+import logo from "../../../assets/dj-white.png"
+// import PreLoader from '../components/PreLoader'
 
 const LoaderPage = () => {
   return (
 
     <>
-    <div className=' bg-black h-[100vh] '>
-      <PreLoader/>  
+    <div className=' bg-black w-full h-screen '>
+    
+    <div className=' w-full'>
+
+    <img
+            src={logo}
+            alt="nature image"
+    />
     </div>
+      <PreLoader />  
+      
+    </div>
+    
     </>
     
   )
