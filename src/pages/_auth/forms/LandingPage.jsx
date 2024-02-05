f5219f77c7fa484a5dc166ba607ab0aae5ef0f1a
Aadesh Motghare <109069284+AadeshMotghare@users.noreply.github.com>
2024-02-05 10:07:57 +0530

    Aadesh

--- src/pages/_auth/forms/LandingPage.jsx
+++ src/pages/_auth/forms/LandingPage.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import Navbar from '../../../components/Navbar'
+import React from "react";
+import Navbar from "../../../components/Navbar";
+import "../../../styles/landing.css";
+import { Link } from "react-router-dom";
+
 
 const LandingPage = () => {
   return (
     <section className=" w-screen h-screen overflow-y-scroll no-scrollbar">
       <div className="sticky">
-      <Navbar />
-
+        <Navbar />
       </div>
 
       <div className=" mt-16">
-
+      <div className="banner-img">
+        <div className="title">
+          <h3>
+            Find the <span>Right Job</span> In the
+            <br />
+            <span> Right Companies</span>
+          </h3>
+          <div className="small-tagline">
+            <p>Got fired..!! Get Ready to be hired</p>
+          </div>
         </div>
-      
+        <div className="button" data-testid="btn">
+          <Link to="/Jobs">Browse Jobs</Link>
+        </div>
+      </div>
+
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default LandingPage
-
+export default LandingPage;
